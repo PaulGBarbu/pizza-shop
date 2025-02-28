61cdba8f4c77bd1da6ff389c02254393caccce67
PaulGBarbu <paulgbarbu@gmail.com>
2025-02-28 11:06:24 +0100

    add pinia to project, move Icons to index.vue instead of Toppings.vue

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -2,5 +2,5 @@
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss']
+  modules: ['@nuxt/ui', '@nuxtjs/tailwindcss', '@pinia/nuxt']
 })
